d2674b7475a00481dbaa9baa589450e6191aaa4a
Olivier Thierry <olivier.thierry@gmail.com>
2023-08-13 17:23:48 +0200

    Use TMDB API

--- ssg/src/types.ts
+++ ssg/src/types.ts
@@ -1,5 +1,21 @@
-export interface Movie {
-  id: string;
+export type MovieResume = {
+  id: number;
   title: string;
-  resume: string;
-}
+  overview: string;
+  releaseDate: string;
+  popularity: number;
+  posterPath: string;
+};
+
+export type MovieDetails = MovieResume & {
+  genres: Array<string>;
+  originalTitle: string;
+  tagline: string;
+  actors: Array<Actor>;
+};
+
+export type Actor = {
+  id: number;
+  name: string;
+  profilePath: string;
+};
