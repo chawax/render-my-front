a3e193be56287fd8ce20dc6e108b37d6f4901fed
Olivier Thierry <olivier.thierry@gmail.com>
2023-08-18 16:02:10 +0200

    Remove unused code

--- ssr/src/pages/movies/[id].tsx
+++ ssr/src/pages/movies/[id].tsx
@@ -72,46 +72,6 @@ export default function MoviePage({
       </div>
     </article>
   );
-
-  // const imgUrl = `https://image.tmdb.org/t/p/w500${movie.posterPath}`;
-  // return (
-  //   <article>
-  //     <h2>{movie.title}</h2>
-  //     <p>{movie.tagline}</p>
-  //     <Image src={imgUrl} alt={movie.title} width={250} height={375} />
-  //     <p>{movie.overview}</p>
-  //     <p>{movie.releaseDate}</p>
-  //     <p>{movie.popularity}</p>
-  //     {movie.genres ? (
-  //       <section>
-  //         <h3>Genres</h3>
-  //         <ul>
-  //           {movie.genres.map((genre: any) => (
-  //             <li key={genre}>{genre}</li>
-  //           ))}
-  //         </ul>
-  //       </section>
-  //     ) : null}
-  //     {movie.actors ? (
-  //       <section>
-  //         <h3>Casting</h3>
-  //         {movie.actors.map((actor: any) => {
-  //           return (
-  //             <div key={actor.id}>
-  //               <p>{actor.name}</p>
-  //               <Image
-  //                 src={`https://image.tmdb.org/t/p/w200${actor.profilePath}`}
-  //                 alt={actor.name}
-  //                 width={100}
-  //                 height={150}
-  //               />
-  //             </div>
-  //           );
-  //         })}
-  //       </section>
-  //     ) : null}
-  //   </article>
-  // );
 }
 
 export const getServerSideProps: GetServerSideProps<{
