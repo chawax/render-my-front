0b2868ce5da2077f1b14a6a513105a85ad498c8b
Olivier Thierry <olivier.thierry@gmail.com>
2023-08-18 15:42:09 +0200

    Add styles with Tailwind to SSR

--- ssr/src/pages/_app.tsx
+++ ssr/src/pages/_app.tsx
@@ -1,17 +1,16 @@
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>
-        <nav>
-          <Link href="/">
-            <h1>My Movies</h1>
-          </Link>
-        </nav>
+      <header className="bg-black p-4 sticky top-0">
+        <Link href="/">
+          <h1 className="text-3xl text-white font-bold">My Movies</h1>
+        </Link>
       </header>
-      <main>
+      <main className="p-4">
         <Component {...pageProps} />
       </main>
     </>
