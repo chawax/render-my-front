d60403a000460121e6a9843e0366015ca5bddd0b
Olivier Thierry <olivier.thierry@gmail.com>
2023-08-21 14:04:03 +0200

    Fix titles

--- server-side-rendering/src/pages/_app.tsx
+++ server-side-rendering/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>My Movies - Server Side Rendering</title>
+      </Head>
       <header className="bg-black p-4 sticky top-0">
         <Link href="/">
           <h1 className="text-3xl text-white font-bold">My Movies</h1>
