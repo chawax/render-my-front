ee79388d356039a33280914dca8e473160734751
Olivier Thierry <olivier.thierry@gmail.com>
2023-08-23 14:41:41 +0200

    Add favorites to islands architecture

---
+++ islands-architecture/src/components/index.ts
@@ -0,0 +1,5 @@
+import ActorCard from "./ActorCard.astro";
+import FavoriteButton from "./FavoriteButton";
+import MovieCard from "./MovieCard.astro";
+
+export { ActorCard, FavoriteButton, MovieCard };
